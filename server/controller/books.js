d60049b2e1f129979a75246cb362a610fd1b9c8d
Collin Porter <colls.potta@gmail.com>
2020-12-30 11:32:03 -0700

    added controller folder with books file to handle the controller for the books

---
+++ server/controller/books.js
@@ -0,0 +1,34 @@
+const db = require('../models');
+
+module.exports = {
+  findAll: function(req, res) {
+    db.Book
+      .find(req.query)
+      .sort({ date: -1 })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  findById: function(req, res) {
+    db.Book
+      .findById(req.params.id)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  create: function(req, res) {
+    db.Book
+      .create(req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  update: function(req, res) {
+    db.Book
+      .findOneAndUpdate({ id: req.params.id }, req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  remove: function(req, res) {
+    db.Book
+      .deleteOne({ bookID: req.params.id })
+      .catch(err => res.status(422).json(err));
+  }
+};
