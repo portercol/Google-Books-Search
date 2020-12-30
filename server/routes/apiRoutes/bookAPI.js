04bec673c2e0a22895dfdd807ebd9d94e71851d8
Collin Porter <colls.potta@gmail.com>
2020-12-30 11:39:46 -0700

    added get, post, put and delete methods for api routes using findall, create, findbyID, update and remove

--- server/routes/apiRoutes/bookAPI.js
+++ server/routes/apiRoutes/bookAPI.js
@@ -1,3 +1,16 @@
 // import express router and controller handling books
 const router = reequire('express').Router();
 const bookController = require('../../controller/books');
+
+// route hit for books
+router.route('/')
+    .get(bookController.findAll)
+    .post(bookController.create);
+
+// route hit for book with specific ID
+router.route('/:id')
+    .get(bookController.findById)
+    .put(bookController.update)
+    .delete(bookController.remove);
+
+module.exports = router;
