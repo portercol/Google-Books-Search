3b09ce3639d44796100a7e42c5a51ab191c0c5f9
Collin Porter <colls.potta@gmail.com>
2020-12-30 11:51:07 -0700

    added comments to explain code

--- server/controller/books.js
+++ server/controller/books.js
@@ -1,6 +1,9 @@
+// create var to store all models data
 const db = require('../models');
 
+// create function to export out of books.js
 module.exports = {
+  // use findAll to find all books
   findAll: function(req, res) {
     db.Book
       .find(req.query)
@@ -8,24 +11,28 @@ module.exports = {
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
+  // use findById to find specific book by ID
   findById: function(req, res) {
     db.Book
       .findById(req.params.id)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
+  // use create to create a new book
   create: function(req, res) {
     db.Book
       .create(req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
+  // use update to find book by ID and update/save
   update: function(req, res) {
     db.Book
       .findOneAndUpdate({ id: req.params.id }, req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
+  // use remove to find book by ID and delete
   remove: function(req, res) {
     db.Book
       .deleteOne({ bookID: req.params.id })
