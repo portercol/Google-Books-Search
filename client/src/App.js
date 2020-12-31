eb79abf0a9f6542970af93a55e728dcf5dd2ca13
Collin Porter <colls.potta@gmail.com>
2020-12-31 13:40:06 -0700

    hid eslintcache, connected front end and back end using a proxy, updated app.js with template data, deacticated webvitals

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,15 @@
 import './App.css';
+import axios from 'axios';
 
 function App() {
+console.log("found");
+  axios.get('/api/books/1984') .then( (res) => {
+    console.log("found a book", res);
+  })
+
   return (
     <div className="App">
+      <h1>HELOOOOOOOO</h1>
     </div>
   );
 }
