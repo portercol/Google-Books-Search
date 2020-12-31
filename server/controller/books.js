e7246b1e64ad0fb3937c07d45e31033f8056bbbb
Collin Porter <colls.potta@gmail.com>
2020-12-31 12:05:28 -0700

    updated ENV in gitignore, changed error status in controller and parameter for .then

--- server/controller/books.js
+++ server/controller/books.js
@@ -6,36 +6,36 @@ module.exports = {
   // use findAll to find all books
   findAll: function(req, res) {
     db.Book
-      .find(req.query)
+      .find(req.body)
       .sort({ date: -1 })
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
+      .then(bookModel => res.json(bookModel))
+      .catch(err => res.status(404).json(err));
   },
   // use findById to find specific book by ID
   findById: function(req, res) {
     db.Book
       .findById(req.params.id)
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
+      .then(bookModel => res.json(bookModel))
+      .catch(err => res.status(404).json(err));
   },
   // use create to create a new book
   create: function(req, res) {
     db.Book
       .create(req.body)
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
+      .then(bookModel => res.json(bookModel))
+      .catch(err => res.status(404).json(err));
   },
   // use update to find book by ID and update/save
   update: function(req, res) {
     db.Book
       .findOneAndUpdate({ id: req.params.id }, req.body)
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
+      .then(bookModel => res.json(bookModel))
+      .catch(err => res.status(404).json(err));
   },
   // use remove to find book by ID and delete
   remove: function(req, res) {
     db.Book
       .deleteOne({ bookID: req.params.id })
-      .catch(err => res.status(422).json(err));
+      .catch(err => res.status(404).json(err));
   }
 };
